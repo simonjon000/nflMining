126628027d2c5e78cee6296dd9cd82b549364f5d
simonjon000 <37024440+simonjon000@users.noreply.github.com>
2018-04-29 17:56:56 -0700

    Add files via upload

---
+++ getTeams.py
@@ -0,0 +1,19 @@
+import csv
+yteam = {}
+team = []
+with open('nfl_elo.csv', 'r') as nflstat:
+    filereader = csv.reader(nflstat)
+    s=""
+    t=1
+    for row in filereader:
+        if t==1:
+            t=0
+            continue
+        if s != row[1]:
+            yteam[s] = set(team)
+            s=row[1]
+            #print(yteam)
+        team.append(row[4].lower())
+        team.append(row[5].lower())
+for key in yteam:
+    print(">"+key+"<")
